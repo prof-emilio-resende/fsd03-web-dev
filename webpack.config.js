f055562c4f36c5ce7cb3acf369348a664decf4d3
Emilio Resende <emilio.murta@msitbrasil.com>
2022-03-28 20:15:10 -0300

    scss[sass] webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = (env, argv) => {
     let htmlPlugin = null;
@@ -15,10 +16,14 @@ module.exports = (env, argv) => {
         module: {
             rules: [
                 {
-                    test: /\.css$/i,
-                    use: [MiniCssExtractPlugin.loader, 'css-loader']
-                }
+                    test: /\.s[ac]ss$/i,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                },
             ]
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [new CssMinimizerPlugin()]
         },
         mode: argv.mode
     };
